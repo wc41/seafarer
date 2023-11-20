8968e66ba5836b0f9aad76463fae763769916d57
wc41 <willcai@seas.upenn.edu>
2023-11-20 14:46:31 -0500

    sky timing fix

--- CIS 5660 Final Project/Assets/Debby-Test/Procedural Sky/SkyManager.cs
+++ CIS 5660 Final Project/Assets/Debby-Test/Procedural Sky/SkyManager.cs
@@ -18,7 +18,7 @@ public class SkyManager : MonoBehaviour
 
             //(Replace with a reference to the game time)
             TimeOfDay += Time.deltaTime;
-            TimeOfDay %= 36; //Modulus to ensure always between 0-24
+            TimeOfDay %= 24; //Modulus to ensure always between 0-24
             // update sky color 
             Material skyColor = GetComponent<Renderer>().sharedMaterial; 
             skyColor.SetTextureOffset("_MainTex", new Vector2((TimeOfDay - 12) * .05f, 0));
